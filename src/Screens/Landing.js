6980c0a6c979c90128b2937319dd941020e2c945
Noah <bitterhippo@gmail.com>
2022-03-26 08:07:02 +0900

    initial commit
    project set up and ready to go

---
+++ src/Screens/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      test landing page
+    </div>
+  )
+};
+
+export default Landing;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Landing from './Screens/Landing';
+
+const App = () => {
+  return <Landing />
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
