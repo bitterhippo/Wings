6619ab01e94775feeb327dbe90b929d36ab281b7
Noah <bitterhippo@gmail.com>
2022-04-03 23:44:05 +0900

    added webpack support for images

--- src/Components/Images/BackgroundImg.js
+++ src/Components/Images/BackgroundImg.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
+//Subcomponents
+import tinyCait from "./tinyCait.png"
+
 export const BackgroundImage = () => { 
   return (
-    <>
-    </>
+    <img 
+    style={styles.imgContainer} 
+    src={tinyCait}
+    />
   )
 };
 
 const styles = {
-
+  imgContainer: {
+    height: 100,
+    width: 100
+  }
 };
 
