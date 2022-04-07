71ee84e2686d5cb81c24263c99102d9df9bcf17b
Noah <bitterhippo@gmail.com>
2022-04-07 17:09:11 +0900

    Update SignUp.js

--- src/Screens/SignUp.js
+++ src/Screens/SignUp.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { ContentWrapper } from '../Components/PPC/ContentWrapper';
+
 export const SignUp = () => {
   return (
-    <>
+    <ContentWrapper>
       Sign up
-    </>
+    </ContentWrapper>
   )
 };
