4a98f5d83a957e55c30bcf7daa49726720dd4f90
Noah <bitterhippo@gmail.com>
2022-05-03 22:48:41 +0900

    active deployment

--- src/Defaults/Tools.js
+++ src/Defaults/Tools.js
@@ -19,7 +19,7 @@ export const ToolDefaults = {
       {
         name: 'tonberryTest',
         jobs: { whm: 5, rdm: 0, sch: 3, warrior: 2 },
-        currentJob: {blm: 30, mnk: 15},
+        currentJob: {primary: ['blm', 30], secondary: ['mnk', 15]},
         faction: {faction: 'Windurst', rank: 2},
         zone: 'South Gustaberg',
       }
